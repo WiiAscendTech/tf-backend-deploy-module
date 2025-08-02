a6d7405df80e4a5fbfc3794c40f8f7ab95eb340d
João Souza <ms.joao.felipe@gmail.com>
2025-08-02 12:39:37 -0300

    feat: update modules

--- locals.tf
+++ locals.tf
@@ -1,4 +1,38 @@
 locals {
+  adot_container_definition = {
+    name      = var.container_name,
+    image     = var.image,
+    cpu       = var.cpu,
+    memory    = var.memory,
+    essential = false,
+
+    command = ["--config=env:ADOT_CONFIG_CONTENT"],
+
+    portMappings = [
+      { containerPort = 4317, protocol = "tcp" },
+      { containerPort = 4318, protocol = "tcp" }
+    ],
+
+    environment = local.environment_variables,
+
+    logConfiguration = {
+      logDriver = "awslogs",
+      options = {
+        awslogs-group         = var.log_group,
+        awslogs-region        = var.region,
+        awslogs-stream-prefix = var.log_stream_prefix
+      }
+    },
+
+    healthCheck = {
+      command     = ["CMD-SHELL", "curl -f http://localhost:13133/health/status || exit 1"],
+      interval    = 30,
+      timeout     = 5,
+      retries     = 3,
+      startPeriod = 10
+    }
+  }
+
   adot_config_env = {
     name  = "ADOT_CONFIG_CONTENT"
     value = data.local_file.adot_config_content.content
