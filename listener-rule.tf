8e6582f284e9f4fc73f753217b668f35875e3e80
João Souza <ms.joao.felipe@gmail.com>
2025-11-11 19:01:42 -0300

    chore: update terraform modules

--- listener-rule.tf
+++ listener-rule.tf
@@ -4,7 +4,7 @@ resource "aws_lb_listener_rule" "this" {
 
   action {
     type             = "forward"
-    target_group_arn = var.target_group_arn
+    target_group_arn = aws_lb_target_group.this.arn
   }
 
   condition {
