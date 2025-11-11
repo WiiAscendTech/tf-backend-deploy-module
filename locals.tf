178dfb02fb38140e9ea37868d7bd4d005e5ede35
João Souza <ms.joao.felipe@gmail.com>
2025-11-11 19:30:34 -0300

    chore: update terraform modules

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
     logDriver = "awsfirelens"
     options = {
       Name = "forward"
-      Tag  = "adot.{{.Name}}"
+      Tag  = "{{.Name}}"
     }
   } : var.enable_cloudwatch_logs ? {
     logDriver = "awslogs"
@@ -16,39 +16,37 @@ locals {
     }
   } : null
 
-  adot_container_definition = merge({
-    name      = var.container_name,
-    image     = var.image,
-    cpu       = var.adot_cpu,
-    memory    = var.adot_memory,
-    essential = false,
-
-    command = ["--config=env:ADOT_CONFIG_CONTENT"],
-
-    portMappings = [
-      { containerPort = 4317, protocol = "tcp" },
-      { containerPort = 4318, protocol = "tcp" }
-    ],
-
-    environment = local.environment_variables,
-
-    healthCheck = {
-      command     = ["CMD-SHELL", "curl -f http://localhost:13133/health/status || exit 1"],
-      interval    = 30,
-      timeout     = 5,
-      retries     = 3,
-      startPeriod = 10
-    }
-  }, local.adot_log_configuration != null ? {
-    logConfiguration = local.adot_log_configuration
-  } : {})
-
   adot_config_env = {
     name  = "ADOT_CONFIG_CONTENT"
     value = data.local_file.adot_config_content.content
   }
 
   environment_variables = concat(var.environment_variables, [local.adot_config_env])
+
+  adot_container_definition = merge({
+    name      = var.container_name
+    image     = var.image
+    cpu       = var.adot_cpu
+    memory    = var.adot_memory
+    essential = false
+
+    command = ["--config=env:ADOT_CONFIG_CONTENT"]
+
+    portMappings = [
+      { containerPort = 4317, hostPort = 4317, protocol = "tcp" },
+      { containerPort = 4318, hostPort = 4318, protocol = "tcp" }
+    ]
+
+    environment = local.environment_variables
+
+    healthCheck = {
+      command     = ["CMD-SHELL", "curl -f http://localhost:13133/health/status || exit 1"]
+      interval    = 30
+      timeout     = 5
+      retries     = 3
+      startPeriod = 10
+    }
+  }, local.adot_log_configuration != null ? { logConfiguration = local.adot_log_configuration } : {})
 
   final_name = var.name_override != null ? var.name_override : "${var.application}-${var.environment}"
 
